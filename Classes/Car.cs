732a28667320873a4024d8231b37484f7c377db3
Thai Vo <tahrbo@gmail.com>
2025-07-29 05:38:13 -0500

    retry

---
+++ Classes/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car() { }
+
+        public string Make;
+        public string Model;
+        public int Year;
+    }
+}

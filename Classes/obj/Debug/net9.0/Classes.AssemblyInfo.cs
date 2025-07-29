62b49d79ddae0f9aad7dc88b5e74b2e4646e77b7
Thai Vo <tahrbo@gmail.com>
2025-07-29 05:34:08 -0500

    ready

--- Classes/obj/Debug/net9.0/Classes.AssemblyInfo.cs
+++ Classes/obj/Debug/net9.0/Classes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea28fd5330180577352ceb700614998a6ba8ba32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ccb41d0a6b6eac383a9d8aa0fea1593836ec51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
